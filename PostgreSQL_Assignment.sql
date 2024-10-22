811ac41595834b65d5583ee2422fff0fbafdadb2
Ahmed Ullah <gulamjoy321@gmail.com>
2024-10-22 19:46:26 +0600

    inserting sample data in tables added

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -25,3 +25,35 @@ CREATE TABLE enrollment (
     student_id INT REFERENCES students(student_id),
     course_id INT REFERENCES courses(course_id)
 );
+
+
+-- inserting sample data in students table
+INSERT INTO students(student_name, age, email, frontend_mark, backend_mark, status)
+VALUES
+('Sameer', 21, 'sameer@example.com', 48, 60, NULL),
+('Zoya', 23, 'zoya@example.com', 52, 58, NULL),
+('Nabil', 22, 'nabil@example.com', 37, 46, NULL),
+('Rafi', 24, 'rafi@example.com', 41, 40, NULL),
+('Sophia', 22, 'sophia@example.com', 50, 52, NULL),
+('Hasan', 23, 'hasan@gmail.com', 43, 39, NULL);
+
+
+-- inserting sample data in courses table
+INSERT INTO courses (course_name, credits)
+VALUES
+('Next.js', 3),
+('React.js', 4),
+('Databases', 3),
+('Prisma', 3);
+
+
+-- inserting sample data in enrollment table
+INSERT INTO enrollment (student_id, course_id)
+VALUES
+(1, 1),
+(1, 2),
+(2, 1),
+(3, 2);
+
+
+
